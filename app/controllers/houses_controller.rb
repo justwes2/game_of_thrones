b5a390e1ffcafee8bd58c74483e28319be1631e4
Wes <wes.coffay@gmail.com>
2017-02-24 10:33:41 -0500

    C and R  in place for houses

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,10 +1,33 @@
 class HousesController < ApplicationController
-  def new
 
+  def index
+    @houses = House.all
+  end
+
+  def show
+    @house = House.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
   end
 
   def create
-    render plain: params[:house].inspect
+    @house = House.new(house_params)
+    @house.save
+    redirect_to @house
   end
 
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def house_params
+      params.require(:house).permit(:name, :words, :sigil_url, :territory)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resource :houses
+  resources :houses
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
